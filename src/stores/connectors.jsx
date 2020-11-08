a0bdef6aebdcbe9040c3461dff46db75b80ad18b
UnderGCrypto <73098714+UnderGCrypto@users.noreply.github.com>
2020-11-08 22:28:55 +0200

    Update connectors.jsx

--- src/stores/connectors.jsx
+++ src/stores/connectors.jsx
@@ -3,7 +3,7 @@ import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
 
 const POLLING_INTERVAL = 12000;
 const RPC_URLS = {
-  56: 'https://bsc-dataseed.binance.org/',
+  97: 'https://bsc-dataseed.binance.org/',
 };
 
 export const injected = new InjectedConnector({
@@ -11,7 +11,7 @@ export const injected = new InjectedConnector({
 });
 
 export const walletconnect = new WalletConnectConnector({
-  rpc: { 56: RPC_URLS[56] },
+  rpc: { 97: RPC_URLS[56] },
   bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
   pollingInterval: POLLING_INTERVAL,
